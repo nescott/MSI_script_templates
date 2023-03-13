acab6579002e6cca7ac11b0cfc08a10b8e4d9bb7
Nancy Scott <scot0854@umn.edu>
2023-03-13 09:22:32 -0500

    Fixed mean calculation to exclude mtDNA

--- depth_calculation/gc_correct_mosdepth.sh
+++ depth_calculation/gc_correct_mosdepth.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem=10gb
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=scot0854@umn.edu
-#SBATCH --time=3:00:00
+#SBATCH --time=2:00:00
 #SBATCH -p msismall,msilarge
 #SBATCH -o %x_%u_%j.out
 #SBATCH -e %x_%u_%j.err
@@ -17,14 +17,16 @@ set -o pipefail
 
 module use "$HOME"/modulefiles.local
 module load deeptools/20221013 # computes and corrects GC bias
-# Requires effective genome size and reference genome in 2bit format
+# Requires effective genome size and reference genome in 2bit format,
+# generated from faToTwoBit and faCount in ~/bin, see README in same dir
 module load mosdepth/20221013 # Calculates depth per tiled window
 
-window=500  #bigger bp window smooths visualization
-ref=sc5314  #short ID
-bam_file=
-genome_size=14320608
+window=500  # bigger bp window smooths visualization
+ref=sc5314  # short ID
+bam_file= # to iterate over as array
+genome_size=14320608  # see above to get this number
 ref2bit="$HOME"/bin/faToTwoBit/C_albicans_SC5314_version_A21-s02-m09-r08_chromosomes.2bit
+mtdna= # contig/scaffold ID for removal so mean correction is only of nuclear genome
 
 # output dirs
 arr=("gc_corrected_bams" "mosdepth_txt" "mosdepth_bed" "tab")
@@ -35,11 +37,9 @@ for d in "${arr[@]}"; do
 done
 
 # for each bam, compute and correct GC bias, calculate depth,
-# calculate mean and perform mean-correction,
+# calculate nuclear coverage mean and perform mean-correction,
 # generate tab-delimited table for graphing,
-# switch to chr number instead of accession scaffold id,
-# generate headers:
-# ready for use with candida karyoploter R scripts
+# ready for use with candida karyoploter R scripts or custom ggplot2 script
 
 in_bam=$(awk -v val="$line" 'NR == val {print $0}' $bam_file)
 strain=$(basename "$in_bam" | cut -d "_" -f 1,2)
@@ -54,10 +54,15 @@ correctGCBias -b "$in_bam" --effectiveGenomeSize "${genome_size}" -g "${ref2bit}
 mosdepth -T 1,10,50,100,200 -n --by "${window}" -t 4 \
 "${strain}_${ref}_deeptools_${window}" gc_corrected_bams/"${strain}_${ref}_deeptools.bam"
 
-mean=$(zcat "${strain}_${ref}_deeptools_${window}".regions.bed.gz \
+zcat "${strain}_${ref}_deeptools_${window}".regions.bed.gz \
+| grep -v "${mtdna}" > "${strain}_${ref}_deeptools_${window}".nuclear.bed
+
+gzip "${strain}_${ref}_deeptools_${window}".nuclear.bed
+
+mean=$(zcat "${strain}_${ref}_deeptools_${window}".nuclear.bed.gz \
 | awk '{total += $4} END { print total/NR}')
 
-zcat "${strain}_${ref}_deeptools_${window}".regions.bed.gz \
+zcat "${strain}_${ref}_deeptools_${window}".nuclear.bed.gz \
 | awk -v val="$mean" 'BEGIN{OFS="\t"} {print $1,$2,$3,$4/val}' \
 > "${strain}_${ref}_deeptools_${window}"_mosdepth.gg.tab
 
--- depth_calculation/mosdepth.sh
+++ depth_calculation/mosdepth.sh
@@ -16,16 +16,13 @@ set -o pipefail
 # local modules
 
 module use "$HOME"/modulefiles.local
-module load deeptools/20221013 # computes and corrects GC bias
-# Requires effective genome size and reference genome in 2bit format
 module load mosdepth/20221013 # Calculates depth per tiled window
 
 line=${SLURM_ARRAY_TASK_ID}
-window=500  #bigger bp window smooths visualization
-ref=sc5314  #short ID
-bam_file=calbicans.bam
-#genome_size=14320608
-#ref2bit="$HOME"/bin/faToTwoBit/C_albicans_SC5314_version_A21-s02-m09-r08_chromosomes.2bit
+window=500  # bigger bp window smooths visualization
+ref=sc5314  # short ID
+bam_file=calbicans.bam # text file listing bams with paths
+mtdna= # contig/scaffold ID for removal so mean correction is only of nuclear genome
 
 # output dirs
 arr=("mosdepth_txt" "mosdepth_bed" "tab")
@@ -35,38 +32,30 @@ for d in "${arr[@]}"; do
   fi
 done
 
-# for each bam, compute and correct GC bias, calculate depth,
-# calculate mean and perform mean-correction,
+# calculate uncorrected nuclear mean and perform mean-correction,
 # generate tab-delimited table for graphing,
-# switch to chr number instead of accession scaffold id,
-# generate headers:
-# ready for use with candida karyoploter R scripts
+# ready for use with candida karyoploter R scripts or custom ggplot2 scripts
 
 in_bam=$(awk -v val="$line" 'NR == val {print $0}' $bam_file)
 strain=$(basename "$in_bam" | cut -d "_" -f 1,2)
 
-#computeGCBias -b "$in_bam" --effectiveGenomeSize "${genome_size}" -g "${ref2bit}" \
-#-o gc_corrected_bams/"${strain}_${ref}_freq.txt"
-
-#correctGCBias -b "$in_bam" --effectiveGenomeSize "${genome_size}" -g "${ref2bit}" \
-#-freq gc_corrected_bams/"${strain}_${ref}_freq.txt" \
-#-o gc_corrected_bams/"${strain}_${ref}_deeptools.bam"
-
 mosdepth -T 1,10,50,100,200 -n --by "${window}" -t 4 \
-"${strain}_${ref}_${window}" "${in_bam}"
+"${strain}_${ref}_deeptools_${window}" gc_corrected_bams/"${strain}_${ref}_deeptools.bam"
 
-mean=$(zcat "${strain}_${ref}_${window}".regions.bed.gz \
+zcat "${strain}_${ref}_deeptools_${window}".regions.bed.gz \
+| grep -v "${mtdna}" > "${strain}_${ref}_deeptools_${window}".nuclear.bed
+
+gzip "${strain}_${ref}_deeptools_${window}".nuclear.bed
+
+mean=$(zcat "${strain}_${ref}_deeptools_${window}".nuclear.bed.gz \
 | awk '{total += $4} END { print total/NR}')
 
-zcat "${strain}_${ref}_${window}".regions.bed.gz \
+zcat "${strain}_${ref}_deeptools_${window}".nuclear.bed.gz \
 | awk -v val="$mean" 'BEGIN{OFS="\t"} {print $1,$2,$3,$4/val}' \
-> "${strain}_${ref}_${window}"_mosdepth.gg.tab
-
-sed -f "sed_${ref}_chr" < "${strain}_${ref}_${window}"_mosdepth.gg.tab \
-> "${strain}_${ref}_${window}"_chrnum_mosdepth.gg.tab
+> "${strain}_${ref}_deeptools_${window}"_mosdepth.gg.tab
 
 sed -i "1s/^/chr\tstart\tstop\t${strain}\n/" \
-"${strain}_${ref}_${window}"_chrnum_mosdepth.gg.tab
+"${strain}_${ref}_deeptools_${window}"_mosdepth.gg.tab
 
 mv ./"${strain}"_*.txt mosdepth_txt
 mv ./"${strain}"_*.tab tab
