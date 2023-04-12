0a4c6241ef3e48d539f89381db1607604269a026
Nancy Scott <scot0854@umn.edu>
2023-04-12 14:27:07 -0500

    adding scratch dir for gc_bams

--- read_depth_and_snp_density/gc_correct.sh
+++ read_depth_and_snp_density/gc_correct.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem=4gb
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=scot0854@umn.edu
-#SBATCH --time=1:00:00
+#SBATCH --time=30
 #SBATCH -p msismall,msilarge
 #SBATCH -o %x_%u_%j.out
 #SBATCH -e %x_%u_%j.err
@@ -23,6 +23,7 @@ module load deeptools/20221013 # computes and corrects GC bias
 line=${SLURM_ARRAY_TASK_ID}
 ref=sc5314  # short ID
 bam_file=bam.files # to iterate over as array
+gc_dir=/scratch.global/scot0854/calbicans/gc_corrected_bams/
 genome_size=14320608
 ref2bit="$HOME"/bin/faToTwoBit/C_albicans_SC5314_version_A21-s02-m09-r08_chromosomes.2bit
 
@@ -30,11 +31,11 @@ ref2bit="$HOME"/bin/faToTwoBit/C_albicans_SC5314_version_A21-s02-m09-r08_chromos
 # generate tab-delimited table for plotting and downstream analysis
 
 in_bam=$(awk -v val="$line" 'NR == val {print $0}' $bam_file)
-strain=$(basename "$in_bam" | cut -d "_" -f 1,2)
+strain=$(basename "$in_bam" | cut -d "_" -f 1)
 
 computeGCBias -b "$in_bam" --effectiveGenomeSize "${genome_size}" -g "${ref2bit}" \
--o gc_corrected_bams/"${strain}_${ref}_freq.txt"
+-o "${gc_dir}${strain}_${ref}_freq.txt"
 
 correctGCBias -b "$in_bam" --effectiveGenomeSize "${genome_size}" -g "${ref2bit}" \
--freq gc_corrected_bams/"${strain}_${ref}_freq.txt" \
--o gc_corrected_bams/"${strain}_${ref}_deeptools.bam"
+-freq "${gc_dir}${strain}_${ref}_freq.txt" \
+-o "${gc_dir}${strain}_${ref}_deeptools.bam"
